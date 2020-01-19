8d28ea35567635532d3b631fae543e2e1b1619a8
RipleyCJ <corey.ripley08@gmail.com>
2020-01-18 16:28:58 -0800

    Multiple changes to address issues #2 , and #3
    Multiple changes to address issues #2 , and #3
    removed download card call, and instead just call urllib.request directly. This way, a variable can be passed to it for the card quanitity a bit easier.
    
    Created a "get_all_card" function that serves as a better way to get all the card data from a deck Allows for capturing the card name, and improves speed of the program.

--- getDeck.py
+++ getDeck.py
@@ -4,75 +4,105 @@ import urllib.request
 import os
 import re
 import shutil
+import time
 
-deck_list = 35187 # enter a deck list number here to download this deck
+deck_list = 35187  # enter a deck list number here to download this deck
 
-base_folder = "/your/folder/here" #example: /users/ripleycj/documents/destinydiceproject/
-os.chdir(base_folder)#changes the directory to the base of the project
-
+base_folder = "your/folder/here/"  # example: /users/ripleycj/documents/destinydiceproject/
+create_die = False  # True if you want the .scad files for 3d printing dice.
 
 """Creates card and dice folder - Deletes them if they already exist"""
 
-if os.path.exists (f'{base_folder}cards/') == True:
+if os.path.exists(f'{base_folder}cards/') == True:
     shutil.rmtree(f'{base_folder}cards/')
 
-if os.path.exists(f'{base_folder}dice/')   == True:
+if os.path.exists(f'{base_folder}dice/') == True:
     shutil.rmtree(f"{base_folder}dice/")
-
 
 try:
     create_card_file = os.mkdir(f'{base_folder}cards')
 except OSError:
-    print ("creation of the folder failed")
+    print("creation of the folder failed")
 
 try:
     create_dice_file = os.mkdir(f'{base_folder}dice/')
 except:
-    print ("creation of the folder failed")
-
+    print("creation of the folder failed")
 
 # path to the now created card and dice folder
 card_path = (f'{base_folder}cards/')
 dice_path = (f'{base_folder}dice/')
 
-create_die = True # True if you want the .scad files for 3d printing dice.
-
-
-
-
-
-
-
-
-
 """This pulls the deck from SW destiny db"""
+
+
 def get_deck():
     response = get(f'http://swdestinydb.com/api/public/decklist/{deck_list}')
-    deck =  json.loads(response.content.decode('utf-8'))
+    cardDeck = json.loads(response.content.decode('utf-8'))
+    deck = []
+    for item in cardDeck['slots']:
+        deck.append(item)
+    return cardDeck
+
+
+def get_all_cards():
+    response = get(f'http://swdestinydb.com/api/public/decklist/{deck_list}')
+    cardDeck = json.loads(response.content.decode('utf-8'))
+
+    deck = []
+    for card in cardDeck['slots']:
+        response = get(f'http://swdestinydb.com/api/public/card/{card}')
+        cardNum = json.loads(response.content.decode('utf-8'))
+        deck.append(cardNum)
+
     return deck
+
+
+def get_card_name(name):
+    # esponse = get(f'http://swdestinydb.com/api/public/card/{item}')
+    # cardNum = json.loads(response.content.decode('utf-8'))
+    return name
+
+
+"""Gets the quantity needed of the card in the deck"""
+
+
+def get_card_quantity(item, cardDeck):
+    quantity = cardDeck['slots'][f'{item}']
+
+    return quantity['quantity']
+
+
+"""Downloads the card to the /cards/ folder"""
+
+
+def download_card(url, item, name, cardDeck):
+    urllib.request.urlretrieve(url, f'{card_path}{name} X{get_card_quantity(item, cardDeck)}.jpg')
+
 
 """Checks if their is a die for the provided card"""
 
+
 def check_die(card_num):
     if card_num == True:
-    	return True
+        return True
     else:
         return False
 
-"""Downloads the card to the /cards/ folder"""
-def download_card(card, item):
-	url = card['imagesrc']
-	urllib.request.urlretrieve(url, f'{card_path}{item}.jpg')
-
 
 """returns the cards die"""
+
+
 def get_die(die):
     return die
 
+
 """Writes to the beggining part of the scad file"""
+
+
 def write_beg_file(card):
     with open("beg_die.txt") as f:
-        beg_file = (card["code"])
+        beg_file = card
         with open(f"{dice_path}{beg_file}.scad", "x") as f1:
             for line in f:
                 f1.write(line)
@@ -80,15 +110,21 @@ def write_beg_file(card):
 
             f1.write('FACES = [\n')
 
+
 """Writes the end of the scad file"""
+
+
 def write_end_file(card):
     with open("end_die.txt") as f:
-        beg_file = card["code"]
+        beg_file = card
         with open(f"{dice_path}{beg_file}.scad", "a") as f1:
             for line in f:
                 f1.write(line)
 
+
 """Checks for blanks and special attack on die"""
+
+
 def check_special(num, card):
     with open(f"{dice_path}{card}.scad", "a") as f:
         if "-" in num:
@@ -98,9 +134,10 @@ def check_special(num, card):
 
 
 """checks for all non special results, aka regular results"""
-def check_not_special(num, card):
 
-        # no special results on die
+
+def check_not_special(num, card):
+    # no special results on die
     die_num = num[0]
     die_str = num.replace(die_num[0], '')
 
@@ -125,12 +162,13 @@ def check_not_special(num, card):
 
 """Checks for sides that cost a resource"""
 
+
 def check_resource_cost(num, card):
     if any(char.isdigit() for char in num) and len(num) > 3 and num[0].isdigit():
         first_num = num[0]
         last_num = num[3]
         pattern = pattern = '[0-9]'
-        die_str = re.sub(pattern,'', num)
+        die_str = re.sub(pattern, '', num)
         with open(f"{dice_path}{card}.scad", "a") as f:
 
             if "R" == die_str:  # resource card
@@ -152,6 +190,8 @@ def check_resource_cost(num, card):
 
 
 """Checks for plus type cards"""
+
+
 def check_plus(num, card):
     if num[0] == "+":
         plus = num[0]
@@ -176,28 +216,32 @@ def check_plus(num, card):
                 f.write(f'         ["disrupt", "{plus}{die_num}"]')
 
 
-
 def get_cards():
-#print (type(deck))
-    for item in get_deck()["slots"]:
-        #gets each card in decklist from swdestinydb
-        get_card = get(f'http://swdestinydb.com/api/public/card/{item}')
+    cardDeck = get_deck()
+    deckCards = get_all_cards()
 
-        #removes file if it already exists - mainly used for testing purposes, but also helps if you are accidentally creating a duplicate
+    for card_list in deckCards:
+        for card in card_list:
+            if card == 'code':
+                item = card_list['code']
+            if card == 'imagesrc':
+                url = card_list['imagesrc']
+            if card == 'name':
+                name = card_list['name']
+
+        # removes file if it already exists - mainly used for testing purposes, but also helps if you are accidentally creating a duplicate
         if os.path.exists(f"{card_path}{item}.scad"):
             os.remove(f"{card_path}{item}.scad")
 
         if os.path.exists(f"{dice_path}{item}.scad"):
             os.remove(f"{dice_path}{item}.scad")
 
-        card = json.loads(get_card.content.decode('utf-8'))
-
         die_count = 1
-        if create_die == True: #change to false if you just want to download cards.
-            if check_die(card["has_die"]):
-                die = get_die(card["sides"])
+        if create_die == True:  # change to false if you just want to download cards.
+            if check_die(card_list["has_die"]):
+                die = get_die(card_list["sides"])
 
-                write_beg_file(card)
+                write_beg_file(item)
 
                 for i in die:  # iterates over all 6 sides
                     check_special(i, item)  # checks for blank side, or special ability side
@@ -216,7 +260,10 @@ def get_cards():
 
                 write_end_file(card)
 
-        download_card(card, item)  # downloads the card from swdestinydb
+        quantity = get_card_quantity(item, cardDeck)
+        urllib.request.urlretrieve(url, f'{card_path}{name} X{quantity}.jpg')
 
 
+start_time = time.time()  # for testing run time of program
 get_cards()
+print("--- %s seconds ---" % (time.time() - start_time))
